1d2b1d6f7b7434d08eaf16a9a2b192be4b740c60
seunsods <seunsods@gmail.com>
2016-05-17 16:57:54 +0100

    Add .xml_files

---
+++ app/src/main/java/com/example/seun/gitsexample/GitsExampleActivity.java
@@ -0,0 +1,13 @@
+package com.example.seun.gitsexample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GitsExampleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gits_example);
+    }
+}
